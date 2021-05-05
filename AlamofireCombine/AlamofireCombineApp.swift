693647ed4c302854d41ee5fca30edf915d6074c7
蔡彥佑 <178ssn@gmail.com>
2021-05-05 10:59:55 +0800

    Initial Commit

---
+++ AlamofireCombine/AlamofireCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireCombineApp.swift
+//  AlamofireCombine
+//
+//  Created by 蔡彥佑 on 2021/5/5.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlamofireCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlamofireCombine
+//
+//  Created by 蔡彥佑 on 2021/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
